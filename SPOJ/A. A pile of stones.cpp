5d1b97709f8b32ceeabee39106d8394f67d1231b
Shamima-Rayhana <85000140+Shamima-Rayhana@users.noreply.github.com>
2022-05-10 10:48:14 +0600

    Create A. A pile of stones.cpp

---
+++ SPOJ/A. A pile of stones.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int cnt=0;
+    for(int i=0; i<n; i++)
+    {
+        if(s[i]=='+')
+            cnt++;
+        else
+            cnt--;
+        if(cnt<0)
+            cnt=0;
+    }
+
+    cout<<cnt<<endl;
+    return 0;
+}
