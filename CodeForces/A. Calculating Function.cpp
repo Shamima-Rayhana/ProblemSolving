65780bb4b25c6e761f65d04041a74eac234d1909
Shamima-Rayhana <85000140+Shamima-Rayhana@users.noreply.github.com>
2022-06-16 23:33:05 +0600

    A. Calculating Function.

---
+++ CodeForces/A. Calculating Function.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long int n,sum=0;
+    cin>>n;
+    if(n%2==1)
+        cout<<-(n+1)/2<<endl;
+    else
+
+        cout<<n/2<<endl;
+    return 0;
+}
