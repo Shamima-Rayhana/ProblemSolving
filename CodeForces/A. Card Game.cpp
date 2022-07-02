62bfb4c375ac5e1a20465d4a46261c2876d7622e
Shamima-Rayhana <85000140+Shamima-Rayhana@users.noreply.github.com>
2022-07-02 18:09:48 +0600

    A. Card Game

---
+++ CodeForces/A. Card Game.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,k1,k2;
+        cin>>n>>k1>>k2;
+        int a[k1+5],b[k2+5];
+        for(int i=0;i<k1;i++)
+        {
+            cin>>a[i];
+        }
+        for(int i=0;i<k2;i++)
+        {
+            cin>>b[i];
+        }
+        sort(a,a+k1);
+        sort(b,b+k2);
+        if(a[k1-1]>b[k2-1])
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+    return 0;
+}
